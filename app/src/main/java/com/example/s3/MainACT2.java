fb8d1cc80577fbf80616078410b2b0beb2434ee1
[Shehan Mallawaarachchi] <[shehan96mts@gmail.com]>
2021-05-12 23:45:42 +0530

    new commit

---
+++ app/src/main/java/com/example/s3/MainACT2.java
@@ -0,0 +1,14 @@
+package com.example.s3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainACT2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main_a_c_t2 );
+    }
+}
